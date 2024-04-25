c3f6bcd4c4ec43d3a669c369d208613e33ee4589
Shreyas-29 <sihasaneshreyas@gmail.com>
2024-04-25 23:51:24 +0530

    🚀 Initial commit

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HomeSection } from "@/components";
+
+const HomePage = () => {
+    return (
+        <div className="wrapper flex flex-col items-center justify-center w-full">
+            <HomeSection />
+        </div>
+    )
+};
+
+export default HomePage
